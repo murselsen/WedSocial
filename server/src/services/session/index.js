cd998b37740077c72a698af0ec0c74c8d8f1790b
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-01-04 23:18:21 +0300

    feat: add getSessionById and deleteSession services to session index

--- server/src/services/session/index.js
+++ server/src/services/session/index.js
@@ -1,3 +1,9 @@
 import createSession from "./create-session.service.js";
 
-export { createSession };
+// Get Session By ID Service
+import getSessionById from "./get-session-by-id.service.js";
+
+// Delete Session Service
+import deleteSession from "./delete-session.service.js";
+
+export { createSession, getSessionById, deleteSession };
