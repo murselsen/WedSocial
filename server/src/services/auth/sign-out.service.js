645b088dc5e2720373a1e16009e0e7b9090edaba
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-01-04 23:17:12 +0300

    feat: improve sign-out service with error handling and logging

--- server/src/services/auth/sign-out.service.js
+++ server/src/services/auth/sign-out.service.js
@@ -1,10 +1,36 @@
+import { getSessionById, deleteSession } from "../session/index.js";
+
+import { isValidObjectId } from "mongoose";
+
 import createError from "http-errors";
-const signOut = (sessionId) => {
-  console.log(`Signing out session: ${sessionId}`);
+const signOut = async (sessionId) => {
+  if (!sessionId) {
+    throw createError(400, "❌ | Sign Out Service | Invalid session ID");
+  }
+
+  if (!isValidObjectId(sessionId)) {
+    throw createError(400, "❌ | Sign Out Service | Invalid session ID format");
+  }
+
+  const session = await getSessionById(sessionId);
+
+  await deleteSession(session._id);
 
   if (!sessionId) {
     throw createError(400, "Invalid session ID");
   }
+
+  const checkSession = await getSessionById(sessionId);
+  if (!checkSession) {
+    // Silinme başarısız olduysa hata fırlat
+    throw createError(
+      500,
+      "❌ | Sign Out Service | The session deletion process failed."
+    );
+  }
+
+  console.log("✅ | Sign Out Service | Session deleted successfully.");
+
   return Promise.resolve(true);
 };
 
