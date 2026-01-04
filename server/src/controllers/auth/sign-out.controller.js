b11a1f9f8dbca9aa94e37d01ff402e2a2df04233
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-01-04 23:17:06 +0300

    feat: enhance sign-out controller logging and cookie management

--- server/src/controllers/auth/sign-out.controller.js
+++ server/src/controllers/auth/sign-out.controller.js
@@ -2,10 +2,21 @@ import { signOut } from "../../services/auth/index.js";
 const signOutController = async (req, res, next) => {
   try {
     if (req.cookies.sessionId) {
+      console.log(
+        "✅ | Sign Out Controller | Signing out session ID:",
+        req.cookies.sessionId
+      );
       await signOut(req.cookies.sessionId);
     }
 
-    console.log("✅ | User signed out successfully.");
+    res.clearCookie("sessionId");
+    console.log("✅ | Sign Out Controller | Session cookie cleared.");
+
+    res.clearCookie("refreshToken");
+    console.log("✅ | Sign Out Controller | Refresh token cookie cleared.");
+
+    console.log("✅ | Sign Out Controller | User signed out successfully.");
+
     res.status(204).send();
   } catch (error) {
     console.error("Error during sign-out:", error);
