5913ab9c63a744f8297e8a0c06b758adc7b46671
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-01-04 23:17:22 +0300

    feat: implement delete session service with validation and logging

---
+++ server/src/services/session/delete-session.service.js
@@ -0,0 +1,32 @@
+import createError from "http-errors";
+import { isValidObjectId } from "mongoose";
+
+import Session from "../../db/models/session.model.js";
+
+/**
+ * Deletes a session from the database by its ID.
+ * @async
+ * @param {string} sessionId - The unique identifier of the session to delete.
+ * @throws {Error} Throws a 400 error if sessionId is not provided or invalid.
+ * @returns {Promise<void>} Resolves when the session is successfully deleted.
+ */
+const deleteSession = async (sessionId) => {
+  if (!sessionId) {
+    throw createError(400, "❌ | Session Delete Service | Invalid session ID");
+  }
+  if (!isValidObjectId(sessionId)) {
+    throw createError(
+      400,
+      "❌ | Session Delete Service | Invalid session ID format"
+    );
+  }
+
+  await Session.findByIdAndDelete(sessionId);
+
+  console.log(
+    `✅ | Session Delete Service | Session ${sessionId} deleted successfully.`
+  );
+  return Promise.resolve(true);
+};
+
+export default deleteSession;
