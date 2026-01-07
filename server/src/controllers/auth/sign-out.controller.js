fca7fc4d77556bee12b069133452ce504d1d9f5e
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-01-07 12:20:20 +0300

    feat: streamline logging in sign-out controller

--- server/src/controllers/auth/sign-out.controller.js
+++ server/src/controllers/auth/sign-out.controller.js
@@ -1,23 +1,16 @@
 import { signOut } from "../../services/auth/index.js";
 const signOutController = async (req, res, next) => {
+  console.log("Inside Sign-Out Controller");
   try {
     if (req.cookies.sessionId) {
-      console.log(
-        "✅ | Sign Out Controller | Signing out session ID:",
-        req.cookies.sessionId
-      );
       await signOut(req.cookies.sessionId);
     }
-
     res.clearCookie("sessionId");
-    console.log("✅ | Sign Out Controller | Session cookie cleared.");
 
     res.clearCookie("refreshToken");
-    console.log("✅ | Sign Out Controller | Refresh token cookie cleared.");
-
-    console.log("✅ | Sign Out Controller | User signed out successfully.");
 
     res.status(204).send();
+    console.log("✅ | Sign Out Controller | User signed out successfully.");
   } catch (error) {
     console.error("Error during sign-out:", error);
     next(error);
