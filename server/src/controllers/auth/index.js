ea8d26e75dca306857620e54dfbf57ac40fdfcd6
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-01-07 12:20:04 +0300

    feat: add meController to auth index for user profile management

--- server/src/controllers/auth/index.js
+++ server/src/controllers/auth/index.js
@@ -1,6 +1,10 @@
+// Sign In
 import signInController from "./sign-in.controller.js";
+// Sign Up
 import signUpController from "./sign-up.controller.js";
-
+// Sign Out
 import signOutController from "./sign-out.controller.js";
+// Me
+import meController from "./me.controller.js";
 
-export { signInController, signUpController, signOutController };
+export { signInController, signUpController, signOutController, meController };
