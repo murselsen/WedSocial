37d819cc247bb329fe41a913a4e79186f18ea2ae
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-01-07 12:20:45 +0300

    feat: enhance authentication middleware with detailed error messages

--- server/src/middlewares/authenticate.middleware.js
+++ server/src/middlewares/authenticate.middleware.js
@@ -1,12 +1,34 @@
+import createError from "http-errors";
+
+import Session from "../db/models/session.model.js";
+
 const authenticateMiddleware = (req, res, next) => {
   console.log("Inside authenticateMiddleware");
   try {
     // Authentication logic here
+    if (!req.headers.authorization) {
+      throw createError(
+        401,
+        "❌ | Authenticate Middleware | Authorization header missing"
+      );
+    }
 
+    if (!req.headers.authorization.startsWith("Bearer ")) {
+      throw createError(
+        401,
+        "Authenticate Middleware",
+        "Invalid authorization format"
+      );
+    }
+
+    const token = req.headers.authorization.split(" ")[1];
+
+    if (!token) {
+      throw createError(401, "❌ | Authenticate Middleware | Token missing");
+    }
+
+    console.log("✅ | Authentication successful");
     
-
-
-
     next();
   } catch (error) {
     next(error);
