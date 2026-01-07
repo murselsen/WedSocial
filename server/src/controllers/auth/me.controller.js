1f4adf84a6ac60f5a7c19e97e57b5e77f667a483
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-01-07 14:39:13 +0300

    feat: update log message in Me Controller for better visibility

--- server/src/controllers/auth/me.controller.js
+++ server/src/controllers/auth/me.controller.js
@@ -1,6 +1,5 @@
-// Giriş yapan kullanıcının bilgilerini döner
 const meController = async (req, res, next) => {
-  console.log("Inside Me Controller");
+  console.log("⚠️ | Inside Me Controller");
   try {
     res.status(200).json({
       success: true,
