a7e4a0672a18fe9f934e19af203637fefcbea95f
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-01-08 14:37:55 +0300

    feat: createAppError kullanımı ile hata yönetimi güncellendi

--- server/src/services/auth/sign-out.service.js
+++ server/src/services/auth/sign-out.service.js
@@ -2,19 +2,17 @@ import { getSessionById, deleteSession } from "../session/index.js";
 
 import { isValidObjectId } from "mongoose";
 
-import createError from "http-errors";
+import { createAppError } from "../../utils/index.js";
+import { ERROR_CODES } from "../../constants/index.js";
+
 const signOut = async (sessionId) => {
   if (!sessionId || !isValidObjectId(sessionId)) {
-    throw createError(400, "❌ | Sign Out Service | Invalid session ID ");
+    throw createAppError(ERROR_CODES.VALIDATION.INVALID_ID, "❌ | Sign Out Service | Invalid session ID ");
   }
 
   const session = await getSessionById(sessionId);
 
   await deleteSession(session._id);
-
-  if (!sessionId) {
-    throw createError(400, "Invalid session ID");
-  }
 
   console.log("✅ | Sign Out Service | Session deleted successfully.");
 
