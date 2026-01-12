99836affbd5c98ce64c4f9a4c0fe0ffe568064f2
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-01-13 00:10:51 +0300

    feat: Login sayfasında LoginForm bileşeni eklendi

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,12 @@
+import React from "react";
+
+import LoginForm from "../components/LoginForm/LoginForm";
+
 const Login = () => {
-  return <div>Login Page</div>;
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
 };
 export default Login;
