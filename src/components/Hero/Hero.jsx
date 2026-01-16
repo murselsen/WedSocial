fa865f1e019102545382cf969e6274509d3b9301
Mürsel ŞEN <murselsen_35@hotmail.com>
2026-01-17 00:31:57 +0300

    feat: wrap Hero component content in a div for improved structure

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,10 +6,13 @@ import logo from "../../assets/logo-trans.png";
 const Hero = () => {
   return (
     <section className="hero" id="hero">
-      <img src={logo} alt="Wedding Celebration" className="hero-image" />
-      <h1 className="hero-title">Welcome to WedSocial</h1>
-
-      <p className="hero-subTitle">Your ultimate wedding planning companion.</p>
+      <div className="hero-content">
+        <img src={logo} alt="Wedding Celebration" className="hero-image" />
+        <h1 className="hero-title">Welcome to WedSocial</h1>
+        <p className="hero-subTitle">
+          Your ultimate wedding planning companion.
+        </p>
+      </div>
     </section>
   );
 };
